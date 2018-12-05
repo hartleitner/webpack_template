fda3dc7f33418fa328aa96373794c247981db566
Lorenz Hartleitner <lorenz.hartleitner@gmail.com>
2018-12-05 17:36:46 +0100

    live-server implemented

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
+import "./../node_modules/jquery/dist/jquery.min.js";
+import "./../node_modules/bootstrap/dist/js/bootstrap.min.js";
+import "./../node_modules/popper.js/dist/popper.min.js";
 import "./scss/main.scss";
 import "./scss/card_1.scss";
 import "./js/test.js";
